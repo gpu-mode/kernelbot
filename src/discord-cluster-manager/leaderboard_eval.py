dda77b8fc677b5b7f9e3410ecf445b2821897636
S1ro1 <matej.sirovatka@gmail.com>
2024-12-27 04:57:20 +0100

    Fix: correct duration cast

--- src/discord-cluster-manager/leaderboard_eval.py
+++ src/discord-cluster-manager/leaderboard_eval.py
@@ -89,7 +89,8 @@ float measure_runtime() {
 
     auto end = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<double>(end - start).count() / TIMED_RUNS;
+    using double_duration = std::chrono::duration<double>;
+    auto duration = std::chrono::duration_cast<double_duration>(end - start).count() / TIMED_RUNS;
     std::cout << "submitted kernel runtime: " << duration << " seconds" << std::endl;
     return duration;
 }
