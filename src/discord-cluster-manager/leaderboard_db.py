e47834f9a5444418114162c864d055d8a984bedf
S1ro1 <matej.sirovatka@gmail.com>
2025-02-28 01:07:04 +0100

    Feat: add user_name

--- src/discord-cluster-manager/leaderboard_db.py
+++ src/discord-cluster-manager/leaderboard_db.py
@@ -430,6 +430,7 @@ class LeaderboardDB:
                     r.score,
                     r.runner,
                     s.id,
+                    s.user_name,
                     RANK() OVER (ORDER BY r.score ASC) as rank
                 FROM leaderboard.runs r
                 JOIN leaderboard.submission s ON r.submission_id = s.id
@@ -453,7 +454,8 @@ class LeaderboardDB:
                         s.submission_time,
                         r.score,
                         r.runner,
-                        s.id
+                        s.id,
+                        s.user_name
                     FROM leaderboard.runs r
                     JOIN leaderboard.submission s ON r.submission_id = s.id
                     JOIN leaderboard.leaderboard l ON s.leaderboard_id = l.id
@@ -468,6 +470,7 @@ class LeaderboardDB:
                     score,
                     runner,
                     id,
+                    user_name,
                     RANK() OVER (ORDER BY score ASC) as rank
                 FROM best_submissions
                 ORDER BY score ASC
@@ -484,8 +487,9 @@ class LeaderboardDB:
                 submission_time=submission[2],
                 submission_score=submission[3],
                 gpu_type=gpu_name,
-                rank=submission[6],
                 submission_id=submission[5],
+                user_name=submission[6],
+                rank=submission[7],
             )
             for submission in self.cursor.fetchall()
         ]
