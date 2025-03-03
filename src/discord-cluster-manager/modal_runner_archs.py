0c689ce96cd9f6c7a628d77bc30744357beecf0b
Mark Saroufim <marksaroufim@gmail.com>
2025-03-03 13:31:46 -0800

    Revert "stricter gpu scheduling for a100s and h100s (#201)"
    This reverts commit 5f3741997de697575609a5a29db29087534fc428.

--- src/discord-cluster-manager/modal_runner_archs.py
+++ src/discord-cluster-manager/modal_runner_archs.py
@@ -4,12 +4,11 @@
 
 from modal_runner import app, cuda_image, modal_run_config
 
-gpus = ["T4", "L4", "A100-80GB", "H100!"]
+gpus = ["T4", "L4", "A100", "H100"]
 for gpu in gpus:
-    gpu_slug = gpu.lower().split("-")[0].strip('!')
-    app.function(gpu=gpu, image=cuda_image, name=f"run_cuda_script_{gpu_slug}", serialized=True)(
+    app.function(gpu=gpu, image=cuda_image, name=f"run_cuda_script_{gpu.lower()}", serialized=True)(
         modal_run_config
     )
     app.function(
-        gpu=gpu, image=cuda_image, name=f"run_pytorch_script_{gpu_slug}", serialized=True
+        gpu=gpu, image=cuda_image, name=f"run_pytorch_script_{gpu.lower()}", serialized=True
     )(modal_run_config)
