a492f9d32f64f29fdcc114c0c7a1637ead151e27
Yang Wang <elainewy@meta.com>
2025-11-06 15:48:08 -0800

    add draft_code table for code editor  (#369)

---
+++ src/migrations/20251106_01_kOjGy-draft-code-editor.py
@@ -0,0 +1,21 @@
+"""
+draft_code_editor
+"""
+
+from yoyo import step
+
+__depends__ = {'20250822_01_UtXzl-website-submission'}
+
+steps = [
+    step("""
+        CREATE TABLE IF NOT EXISTS leaderboard.draft_code (
+            id SERIAL PRIMARY KEY,
+            leaderboard_id INTEGER NOT NULL REFERENCES leaderboard.leaderboard(id) ON DELETE CASCADE,
+            user_id TEXT NOT NULL REFERENCES leaderboard.user_info(id) ON DELETE CASCADE,
+            type TEXT NOT NULL DEFAULT 'general',
+            code BYTEA NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            last_modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+        );
+    """)
+]
