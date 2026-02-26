d2015443404b57bd87c0625d7e8fa85a8cdcc376
Yang Wang <elainewy@meta.com>
2026-02-26 12:07:50 -0800

    test1

---
+++ src/migrations/20260226_01_WgYAV-queryindex.py
@@ -0,0 +1,38 @@
+"""
+queryindex
+"""
+
+from yoyo import step
+
+__depends__ = {'20260225_01_aW5Bu-add-leaderboard-performance-indexes'}
+
+
+steps = [
+    step(
+        """
+        CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_runs_valid_scores
+        ON leaderboard.runs (submission_id, runner, score, secret, passed);
+        """,
+        """
+        DROP INDEX CONCURRENTLY IF EXISTS leaderboard.idx_runs_valid_scores;
+        """
+    ),
+    step(
+        """
+        CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_submission_leaderboard_id
+        ON leaderboard.submission (leaderboard_id);
+        """,
+        """
+        DROP INDEX CONCURRENTLY IF EXISTS leaderboard.idx_submission_leaderboard_id;
+        """
+    ),
+    step(
+        """
+        CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_submission_user_id
+        ON leaderboard.submission (user_id);
+        """,
+        """
+        DROP INDEX CONCURRENTLY IF EXISTS leaderboard.idx_submission_user_id;
+        """
+    ),
+]
